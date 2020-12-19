4be5af3b6149b93ce256c02d42f41f257e46968b
Nirav Kumbhariya <niravkumbhariya101@gmail.com>
2020-12-19 22:38:13 +0530

    design update

--- html/js/script.js
+++ html/js/script.js
@@ -1,8 +1,21 @@
 /* Script on scroll
 ------------------------------------------------------------------------------*/
-// $(window).scroll(function() {
+$(window).scroll(function() {
+	if($(this).scrollTop() > 100){
+	  $('header').addClass('sticky');
+	}
+	else{
+	  $('header').removeClass('sticky');
+	}
 
-// });
+	// scroll top
+	var scrollTop = $(".scrollToTop");
+	if ($(this).scrollTop() < 500) {
+    	scrollTop.removeClass("active");
+    } else {
+        scrollTop.addClass("active");
+    }
+});
 
 /* Script on ready
 ------------------------------------------------------------------------------*/
@@ -35,8 +48,8 @@ $(window).ready(function() {
 		$('nav').removeClass("active");
 	});
 
-	//---- owl carousel script ----//
-	$('.owl-carousel').owlCarousel({
+	//---- hero banner owl carousel script ----//
+	$('.hero-banner .owl-carousel').owlCarousel({
 	    loop:true,
 	    margin:10,
 	    nav:false,
@@ -47,5 +60,51 @@ $(window).ready(function() {
 	        }
 	    }
 	});
+
+	//---- about us owl carousel script ----//
+	$('.planning-box .owl-carousel').owlCarousel({
+	    loop:false,
+	    margin:16,
+	    nav:false,
+	    dots: true
+	});
+
+	//---- youtube popup script ----//
+	$(".popup").click(function (e) {
+		e.preventDefault();
+	    var $this = $(this);
+	    var $iframe = $("<iframe>").attr("src", $this.data("link")).css({"width": 600, "height": 400});
+	    $("#video-view").append($iframe);
+	    $iframe.wrap("<div class='video'>");
+	    
+	    if($('body').hasClass("popup-video")){
+			$('body').removeClass("popup-video");
+			
+		}else{
+			$('body').addClass("popup-video");
+		}
+
+		var popup_video = $('#video-view .video, .popup');
+	    $('#wrapper').click(function (e) {
+	        if ((popup_video[0] != e.target) && (!popup_video.has(e.target).length)) 
+	        {
+				$('body').removeClass("popup-video");
+			}
+	    });
+	});
+
+	//---- faq script ----//
+	$("#faq .accordian-list .name").click(function(){
+		$(this).parent().toggleClass('active').siblings().removeClass('active').children('.accordian-list .caption').slideUp('slow');
+		$(this).siblings(".accordian-list .caption").slideToggle("2500");
+	});
+
+	//---- scroll to top script ----//
+    $('.scrollToTop').on('click', function () {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 500);
+        return false;
+    });
 
 });	
